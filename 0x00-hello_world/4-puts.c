b5797346154ef842224a607a7395d20cc0d16dba
Chrissy254 <christabelokumu@gmail.com>
2023-03-26 14:14:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main -prints programming is likebuilding a multilingual puzzle
+*return: always 0
+*/
+int main9(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
