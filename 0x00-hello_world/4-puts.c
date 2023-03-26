7f656525225d5aec97dcbcc88b7756c007ac669f
Chrissy254 <christabelokumu@gmail.com>
2023-03-26 15:51:45 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main -prints programming is likebuilding a multilingual puzzle
-*return: always 0
+* main - a C program that prints exactly Programming is like building a multilingual puzzle
+* return: 0 (success)
 */
 int main9(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
