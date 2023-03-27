36061dd3e5a300e9e2941460ad2ba679fc7173e1
Chrissy254 <christabelokumu@gmail.com>
2023-03-26 17:20:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - a C program that prints exactly Programming is like building a multilingual puzzle
-* return: 0 (success)
+* main - entry point,no return
+* return: 0 (success),
 */
 int main9(void)
 {
