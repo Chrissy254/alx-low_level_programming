81b7c923a29f7a99670544810d99e0418e4b089b
Chrissy254 <christabelokumu@gmail.com>
2023-03-30 02:57:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,12 +7,6 @@
 
 int main(void)
 {
-/**
-*puts - prints the given string and a newline character to stdout
-*
-* Description: This function prin "Programming is like building a multilingual"
-*followed by a new line.
-*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
