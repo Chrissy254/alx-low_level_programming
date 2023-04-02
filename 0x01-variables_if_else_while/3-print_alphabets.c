76019c1b8bff8e69772afbe0546d2defa21b118f
Chrissy254 <christabelokumu@gmail.com>
2023-04-02 12:03:15 -0700

    ./3-print_alphabets | cat -e

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@ int main(void)
 {
 char c;
 /* print lowercase alphabet */
-for (c = 'a'; c <= 'z'; c++
+for (c = 'a'; c <= 'z'; c++)
 putchar(c);
 /* print uppercase alphabet */
 for (c = 'A'; c <= 'Z'; c++)
