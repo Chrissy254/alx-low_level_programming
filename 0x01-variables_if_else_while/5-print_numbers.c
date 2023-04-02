73f58a5416af4cf267659b2d39e6bdb67765df17
Chrissy254 <christabelokumu@gmail.com>
2023-04-02 12:39:04 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+/* print single digit numbers */
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0'); 
+/*convert digit to character and print */
+}
+/* print newline */
+putchar('\n');
+return (0);
+}
+
